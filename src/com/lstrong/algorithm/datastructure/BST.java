b48f1149a5b20de650e0a99af652ff6d7c634abe
lstrong_glb <lstrong_glb@foxmail.com>
2020-12-21 22:18:39 +0800

    version1

--- src/com/lstrong/algorithm/datastructure/BST.java
+++ src/com/lstrong/algorithm/datastructure/BST.java
@@ -1,6 +1,10 @@
 
 package com.lstrong.algorithm.datastructure;
 
+
+
+import java.util.Stack;
+
 /**
  * 一些声明信息
  * Description: <br/>
@@ -11,12 +15,12 @@ package com.lstrong.algorithm.datastructure;
  */
 public class BST<E extends Comparable<E>> {
 
-    private class Node{
+    private class Node {
         public E e;
         public Node left;
         public Node right;
 
-        public Node(E e){
+        public Node(E e) {
             this.e = e;
             left = null;
             right = null;
@@ -26,52 +30,52 @@ public class BST<E extends Comparable<E>> {
     private Node root;
     private int size;
 
-    public BST(){
+    public BST() {
         root = null;
         size = 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public void add(E e){
+    public void add(E e) {
         root = add(root, e);
     }
 
-    private Node add(Node node, E e){
+    private Node add(Node node, E e) {
 
-        if(node == null){
+        if (node == null) {
             size++;
             return new Node(e);
         }
 
-        if(e.compareTo(node.e) < 0){
+        if (e.compareTo(node.e) < 0) {
             node.left = add(node.left, e);
-        }else if(e.compareTo(node.e) > 0){
-            node.right =add(node.right, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
         }
 
         return node;
     }
 
-    public boolean contains(E e){
+    public boolean contains(E e) {
         return contains(root, e);
     }
 
-    private boolean contains(Node node, E e){
+    private boolean contains(Node node, E e) {
 
-        if(node == null) {
+        if (node == null) {
             return false;
         }
 
-        if(e.compareTo(node.e) == 0) {
+        if (e.compareTo(node.e) == 0) {
             return true;
-        } else if(e.compareTo(node.e) < 0) {
+        } else if (e.compareTo(node.e) < 0) {
             return contains(node.left, e);
         } else {
             return contains(node.right, e);
@@ -79,12 +83,12 @@ public class BST<E extends Comparable<E>> {
 
     }
 
-    public void preOrder(){
+    public void preOrder() {
         preOrder(root);
     }
 
-    private void preOrder(Node node){
-        if(node == null){
+    private void preOrder(Node node) {
+        if (node == null) {
             return;
         }
 
@@ -93,12 +97,34 @@ public class BST<E extends Comparable<E>> {
         preOrder(node.right);
     }
 
-    public void inOrder(){
+    private void preOrderNR(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        Stack<Node> nodeStack = new Stack<>();
+        nodeStack.push(node);
+
+        while (!nodeStack.isEmpty()) {
+            Node cur = nodeStack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right != null) {
+                nodeStack.push(cur.right);
+            }
+            if (cur.left != null) {
+                nodeStack.push(cur.left);
+            }
+
+        }
+    }
+
+    public void inOrder() {
         inOrder(root);
     }
 
-    private void inOrder(Node node){
-        if(node == null){
+    private void inOrder(Node node) {
+        if (node == null) {
             return;
         }
 
@@ -107,12 +133,30 @@ public class BST<E extends Comparable<E>> {
         preOrder(node.right);
     }
 
-    public void postOrder(){
+    private void inOrderNR(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        Stack<Node> nodeStack = new Stack<>();
+        Node cur = node;
+        while (cur != null || !nodeStack.isEmpty()){
+            while (cur != null){
+                nodeStack.push(cur);
+                cur = cur.left;
+            }
+            cur = nodeStack.pop();
+            System.out.println(cur.e);
+            cur = cur.right;
+        }
+    }
+
+    public void postOrder() {
         postOrder(root);
     }
 
-    private void postOrder(Node node){
-        if(node == null){
+    private void postOrder(Node node) {
+        if (node == null) {
             return;
         }
 
