6dfd02fce76edbd32f58dfb987f9f8513532e77a
lstrong <lstrong@foxmail.com>
2020-12-22 14:09:57 +0800

    add BST

---
+++ src/com/lstrong/algorithm/datastructure/BSTSet.java
@@ -0,0 +1,38 @@
+package com.lstrong.algorithm.datastructure;
+
+/**
+ * @author Administrator
+ */
+public class BSTSet<E extends Comparable<E>> implements Set<E>{
+
+    private final BST<E> bst;
+
+    public BSTSet(){
+        bst = new BST<>();
+    }
+
+    @Override
+    public void add(E e) {
+        bst.add(e);
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return bst.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return bst.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return bst.isEmpty();
+    }
+}
