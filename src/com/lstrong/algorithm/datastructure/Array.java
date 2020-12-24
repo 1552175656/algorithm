99b8787b007ad758afda23b94688511a71bb8752
lstrong_glb <lstrong_glb@foxmail.com>
2020-12-24 22:57:24 +0800

    Merge branch 'master' of https://github.com/1552175656/algorithm
     Conflicts:
    	.idea/vcs.xml

---
+++ src/com/lstrong/algorithm/datastructure/Array.java
@@ -0,0 +1,108 @@
+package com.lstrong.algorithm.datastructure;
+
+/**
+ * 一些声明信息
+ * Description: <br/>
+ * date: 2020/12/24 21:05<br/>
+ *
+ * @author asus<br />
+ * @since JDK 1.8
+ */
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(0);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+
+        if (size == data.length) {
+            throw new IllegalArgumentException("Add failed. Array is full.");
+        }
+
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Require index >= 0");
+        }
+
+        System.arraycopy(data, index, data, index + 1, size - index);
+        data[index] = e;
+        size++;
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        }
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index is illegal.");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E remove(int index){
+        if(index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove failed. Index is illegal");
+        }
+
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        return ret;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+}
