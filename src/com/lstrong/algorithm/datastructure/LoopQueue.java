46e0ac3b5c6565049e2cb1a80be8bf07fff3e795
lstrong <lstrong@foxmail.com>
2020-12-25 11:21:51 +0800

    add BST

---
+++ src/com/lstrong/algorithm/datastructure/LoopQueue.java
@@ -0,0 +1,89 @@
+package com.lstrong.algorithm.datastructure;
+
+
+import javax.imageio.IIOException;
+
+/**
+ * @author Administrator
+ */
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front;
+    private int tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+        if (size == getCapacity()) {
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+
+        if(isEmpty()){
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        }
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+
+        if(size == getCapacity() / 4 && getCapacity() / 2 != 0){
+            resize(getCapacity() / 2);
+        }
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if(isEmpty()){
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        }
+
+        return data[front];
+    }
+
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+}
