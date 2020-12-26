ab203a420bc8beeeaef7c8f7f1b6d9e2280a40ec
lstrong_glb <lstrong_glb@foxmail.com>
2020-12-26 22:25:18 +0800

    Merge branch 'master' of https://github.com/1552175656/algorithm
     Conflicts:
    	.idea/vcs.xml

--- src/com/lstrong/algorithm/leetcode/medium/solution_75/Solution.java
+++ src/com/lstrong/algorithm/leetcode/medium/solution_75/Solution.java
@@ -12,5 +12,26 @@ public class Solution {
 
     public void sortColors(int[] nums) {
 
+        //[0, zero] == 0, [zero + 1, i - 1] == 1, [two, n - 1] == 2
+        int zero = -1, i = 0, two = nums.length;
+        while (i < two) {
+
+            if (nums[i] == 0) {
+                zero++;
+                swap(nums, i, zero);
+                i++;
+            } else if (nums[i] == 2) {
+                two--;
+                swap(nums, i, two);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
