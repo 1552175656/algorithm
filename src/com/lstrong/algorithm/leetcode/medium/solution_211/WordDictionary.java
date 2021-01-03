2f8d237c0e9ad4f1e4fd5dd0bd864416cf0c5b19
lstrong_glb <lstrong_glb@foxmail.com>
2021-01-03 20:35:23 +0800

    Merge branch 'master' of https://github.com/1552175656/algorithm
     Conflicts:
    	.idea/vcs.xml

---
+++ src/com/lstrong/algorithm/leetcode/medium/solution_211/WordDictionary.java
@@ -0,0 +1,84 @@
+package com.lstrong.algorithm.leetcode.medium.solution_211;
+
+
+import java.util.TreeMap;
+
+/**
+ * 一些声明信息
+ * Description: <br/>
+ * date: 2021/1/2 21:03<br/>
+ *
+ * @author asus<br />
+ * @since JDK 1.8
+ */
+public class WordDictionary {
+
+    private class Node {
+
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<Character, Node>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(Node node, String word, int index) {
+
+        if (index == word.length()) {
+            return node.isWord;
+        }
+
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else {
+            for (char nextChar : node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
