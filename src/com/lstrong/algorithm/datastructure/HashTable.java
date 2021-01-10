04b37f6dcebc21947fe1ce50e58ecdc84828fea6
lstrong_glb <lstrong_glb@foxmail.com>
2021-01-10 22:39:40 +0800

    Merge branch 'master' of https://github.com/1552175656/algorithm
     Conflicts:
    	.idea/vcs.xml

---
+++ src/com/lstrong/algorithm/datastructure/HashTable.java
@@ -0,0 +1,71 @@
+package com.lstrong.algorithm.datastructure;
+
+import java.util.TreeMap;
+
+/**
+ * 一些声明信息
+ * Description: <br/>
+ * date: 2021/1/10 20:01<br/>
+ *
+ * @author asus<br />
+ * @since JDK 1.8
+ */
+public class HashTable<K, V> {
+
+    private TreeMap<K, V>[] hashTable;
+    private int M;
+    private int size;
+
+    public HashTable(int M) {
+        this.M = M;
+        size = 0;
+        hashTable = new TreeMap[M];
+        for (int i = 0; i < M; i++) {
+            hashTable[i] = new TreeMap<>();
+        }
+    }
+
+    public HashTable() {
+        this(97);
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(K key, V value) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+
+        if (map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            map.put(key, value);
+            size++;
+        }
+    }
+
+    public V remove(K key) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        V ret = null;
+        if(map.containsKey(key)){
+            ret = map.remove(key);
+            size--;
+        }
+        return ret;
+    }
+
+    public void set(K key, V value){
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if(!map.containsKey(key)){
+            throw new IllegalArgumentException(key + "doesn't exist!");
+        }
+
+        map.put(key, value);
+    }
+
+
+}
---
+++ src/com/lstrong/algorithm/datastructure/RBTree.java
@@ -0,0 +1,133 @@
+package com.lstrong.algorithm.datastructure;
+
+/**
+ * 一些声明信息
+ * Description: <br/>
+ * date: 2021/1/10 11:46<br/>
+ *
+ * @author asus<br />
+ * @since JDK 1.8
+ */
+public class RBTree<K extends Comparable<K>, V> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        public K key;
+        public V value;
+        public Node left;
+        public Node right;
+        public boolean color;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            left = null;
+            right = null;
+            color = RED;
+        }
+
+
+        @Override
+        public String toString() {
+            return key.toString() + ":" + value.toString();
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public RBTree() {
+        root = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private boolean isRed(Node node) {
+        if (node == null) {
+            return BLACK;
+        }
+        return node.color;
+    }
+
+    private Node leftRotate(Node node) {
+        Node x = node.right;
+
+        node.right = x.left;
+        x.left = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    private Node rightRotate(Node node) {
+
+        Node x = node.left;
+
+        node.left = x.right;
+        x.right = node;
+
+        x.color = node.color;
+        node.color = RED;
+
+        return x;
+    }
+
+    private void flipColors(Node node) {
+
+        node.color = RED;
+        node.left.color = BLACK;
+        node.right.color = BLACK;
+
+    }
+
+
+    public void add(K key, V value) {
+        root = add(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node add(Node node, K key, V value) {
+
+
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+
+        if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = leftRotate(node);
+        }
+
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rightRotate(node);
+        }
+
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
+
+
+        return node;
+    }
+
+
+}
